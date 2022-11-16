e047a1eb52dc2a7d11fc18a48e6314625f5ba920
Bruna Terribile <brunaterribile@gmail.com>
2022-11-16 14:19:19 -0300

    Config. React + Routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import GlobalStyle from './assets/styles/GlobalStyle';
 import Extract from './pages/Extract';
 import Login from './pages/Login';
 import NewEntry from './pages/NewEntry';
-import NewOutput from './pages/NewOutput';
+import NewOutgo from './pages/NewOutgo';
 import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
-      
+      <GlobalStyle />
       <Routes>
-        <Route path='/login' element={<Login />}/>
+        <Route path='/' element={<Login />}/>
         <Route path='/cadastro' element={<SignUp />}/>
         <Route path='/extrato' element={<Extract />}/>
         <Route path='/novaentrada' element={<NewEntry />}/>
-        <Route path='/novasaida' element={<NewOutput />}/>
+        <Route path='/novasaida' element={<NewOutgo />}/>
       </Routes>
 
     </BrowserRouter>
