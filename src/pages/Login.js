b133f84efe9be30f33de41e71a2e742d191d0188
Bruna Terribile <brunaterribile@gmail.com>
2022-11-21 16:29:37 -0300

    Feat: Basic login and sign-up

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,58 @@
 import styled from "styled-components"
 import logo from "../assets/logo.svg"
+import { useState, useEffect } from "react"
+import axios from "axios"
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { PageContainer, Form } from "../assets/styles/style"
 
 export default function Login() {
+    const [email, setEmail] = useState(undefined)
+    const [password, setPassword] = useState(undefined)
+    const navigate = useNavigate()
+
+    function handleLogin(e){
+        e.preventDefault();
+
+        const URL = "http://localhost:5000/sign-in"
+        const body = {
+            email: {email},
+            password: {password}
+        }
+
+        axios.post(URL, body)
+            .then((res) => {
+                navigate("/extract")
+            })
+            .catch((err) => {
+                alert(`Algo deu errado! \n${err.response.data.message} \n`)
+                console.log(err)
+            })
+    }
+   
     return (
         <PageContainer>
             <Logo><img src={logo} alt="logo"/></Logo>
-            <Form>
+            <Form onSubmit={handleLogin}>
                 <input 
                     name="email"
                     placeholder="E-mail"
                     type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
                     required
                 />
                 <input 
                     name="senha"
                     placeholder="Senha"
                     type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
                     required
                     />
-                <button>Entrar</button>
+                <button type="submit">Entrar</button>
             </Form>
-            <Link to="/cadastro">Primeira vez? Cadastre-se!</Link>
+            <Link to="/sign-up">Primeira vez? Cadastre-se!</Link>
         </PageContainer>
     )
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,21 +1,53 @@
 import styled from "styled-components"
 import logo from "../assets/logo.svg"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { PageContainer, Form } from "../assets/styles/style"
+import { useState } from "react"
+import axios from "axios"
 
 export default function SignUp() {
+    const navigate = useNavigate()
+    const [form, setForm] = useState({name: "", email: "", password: ""})
+
+    function handleForm(e) { //Armazena os estados do form conforme o usuário preenche
+        const { name, value } = e.target
+        setForm({ ...form, [name]: value })
+    }
+
+    function registerUser(e) {
+        e.preventDefault()
+
+        const URL = "http://localhost:5000/sign-up"
+        const body = {
+            ...form
+        }
+
+        axios.post(URL, body)
+            .then((res) => {
+                navigate("/sign-in")
+            })
+            .catch((err) => {
+                alert(`Algo deu errado! \n${err.response.data.message} \n`)
+                console.log(err)
+            })
+    }
+
     return (
         <PageContainer>
             <Logo><img src={logo} alt="logo"/></Logo>
-            <Form>
+            <Form onSubmit={registerUser}>
                 <input 
                     name="name"
+                    value={form.name}
+                    onChange={handleForm}
                     placeholder="Nome"
                     type="text"
                     required
                 />
                 <input 
                     name="email"
+                    value={form.email}
+                    onChange={handleForm}
                     placeholder="E-mail"
                     type="email"
                     required
@@ -24,17 +56,18 @@ export default function SignUp() {
                     name="senha"
                     placeholder="Senha"
                     type="password"
+                    value={form.password}
+                    onChange={handleForm}
                     required
                     />
                 <input 
                     name="confirm-senha"
                     placeholder="Confirme a senha"
                     type="password"
-                    required
                 />
                 <button>Cadastrar</button>
             </Form>
-            <Link to="/">Já tem uma conta? Entre agora!</Link>
+            <Link to="/sign-in">Já tem uma conta? Entre agora!</Link>
         </PageContainer>
     )
 }
